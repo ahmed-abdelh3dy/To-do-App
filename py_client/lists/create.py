4830b2cc3871f24b634f9eaf56dfbb312648423f
ahmed-abdelh3dy <ahmedmohhamed236@gmail.com>
2025-07-20 14:16:13 +0200

    create list

--- py_client/lists/create.py
+++ py_client/lists/create.py
@@ -1,14 +1,13 @@
 import requests
 
-
-endpoint = 'http://127.0.0.1:8000/auth/lists'
+headers = {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzUzMDk0NDQyLCJpYXQiOjE3NTMwMDgwNDIsImp0aSI6ImZlOGQxOGE1YWI5ODQ2MGY5OWY1ZWM1MmI2ZjAyMWQzIiwidXNlcl9pZCI6M30.axpkzK-Gk7uvkFYSIbk3qjUEf4c9byHLkUdRj2_L4Wo'}
+endpoint = 'http://127.0.0.1:8000/lists'
 
 data = {
-    'title':'create list one',
+    'title':'create list two',
     'deadline':'2025-10-10'
 }
 
-access = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzUyNzU5MjM5LCJpYXQiOjE3NTI2NzI4MzksImp0aSI6ImQ5OGExY2QxMTZlNTQ3ZmRiZDI4OTgzZTBjYmY1MmJhIiwidXNlcl9pZCI6M30.yxVXgUghek0argsSruhjoFPYCJLEMXeTM5SIX4UURO8'
 
-get_response = requests.get(endpoint  , data=data )
+get_response = requests.post(endpoint  , json=data , headers = headers )
 print(get_response.json())
